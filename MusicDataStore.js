e811ed81d98e084a2c182ef1216529dd99c161a0
Rubyc <759337828@qq.com>
2021-01-06 12:45:36 +0800

    音乐数据的持久化封装

---
+++ MusicDataStore.js
@@ -0,0 +1,36 @@
+const Store = require('electron-store')
+const { v4: uuidv4 } = require('uuid');
+const path = require('path')
+
+class DataStore extends Store {
+  constructor(setting) {
+    super(setting)
+    this.tracks = this.get('tracks') || []
+  }
+ 
+  saveTracks() {
+    this.set('tracks', this.tracks)
+    return this
+  }
+
+  getTracks() {
+    return this.get('trakcs') || []
+  }
+
+  addTracks(tracks) {
+    const tracksWithProps = tracks.map(track => {
+      return {
+        id: uuidv4(),
+        path: track,
+        fileName: path.basename(track)
+      }
+    }).filter(track => {
+      const currentTracksPath = this.getTracks().map(track => track.path)
+      return currentTracksPath.indexOf(track.path) < 0
+    })
+    this.tracks = [ ...this.tracks, ...tracksWithProps]
+    return this.saveTracks()
+  }
+}
+
+module.exports = DataStore
