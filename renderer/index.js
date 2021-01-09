74814ba25321cdb7ffa0d3f44e16e5ffc9411de1
Rubyc <759337828@qq.com>
2021-01-09 15:14:48 +0800

    渲染主页面

--- renderer/index.js
+++ renderer/index.js
@@ -9,11 +9,24 @@ $('add-music-button').addEventListener('click', () => {
 const renderListHTML = (tracks) => {
   const tracksList = $('tracksList')
   const tracksListHTML = tracks.reduce((html, track) => {
-    html += `<li class="music-track list-group-item d-flex">`
+    html += `<li class="row music-track list-group-item d-flex justify-content-between align-items-center">
+      <div class="col-10">
+        <i class="fas fa-music mr-2"></i>
+        <b>${track.fileName}</b>
+      </div>
+      <div class="col-2">
+        <i class="fas fa-play mr-2"></i>
+        <i class="fas fa-trash-alt mr-2"></i>
+      </div>
+    </li>`
+    return html
   }, '')
+  const emptyTrackHTML = '<div class="alert alter-primary">还没有添加音乐</div>'
+  tracksList.innerHTML = tracks.length ? `<ul class="list-group">${tracksListHTML}</ul>` : emptyTrackHTML
 }
 
-ipcRenderer.on('getTracks' , (event, target) => {
+ipcRenderer.on('getTracks' , (event, tracks) => {
   console.log('mainWindow接收到数据')
   // 渲染主页面列表
+  renderListHTML(tracks)
 })
